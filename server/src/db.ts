5097cf515fe993a234beb7439758c20739899c09
vanyadelova <happy.vanya.delova@gmail.com>
2018-12-03 20:33:05 +0100

    First commit

--- server/src/db.ts
+++ server/src/db.ts
@@ -3,7 +3,7 @@ import { DefaultNamingStrategy } from 'typeorm/naming-strategy/DefaultNamingStra
 import { NamingStrategyInterface } from 'typeorm/naming-strategy/NamingStrategyInterface'
 import { snakeCase } from 'typeorm/util/StringUtils'
 import User from './users/entity'
-import { Player, Game } from './games/entities'
+
 
 class CustomNamingStrategy extends DefaultNamingStrategy implements NamingStrategyInterface {
 
@@ -30,8 +30,8 @@ export default () =>
     url: process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres',
     entities: [
       User,
-      Player,
-      Game
+      
+    
     ],
     synchronize: true, // careful with this in production!
     logging: true,
--- server/src/users/entity.ts
+++ server/src/users/entity.ts
@@ -1,8 +1,8 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm'
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
 import { Exclude } from 'class-transformer';
 import { MinLength, IsString, IsEmail } from 'class-validator';
-import * as bcrypt from 'bcrypt'
-import { Player } from '../games/entities';
+import * as bcrypt from 'bcrypt';
+
 
 @Entity()
 export default class User extends BaseEntity {
@@ -41,6 +41,5 @@ export default class User extends BaseEntity {
 
   // this is a relation, read more about them here:
   // http://typeorm.io/#/many-to-one-one-to-many-relations
-  @OneToMany(_ => Player, player => player.user) 
-  players: Player[]
+  
 }
